45d23b4266ce01c213e378ccd10850c32bc218cf
Matthew Hassell <hassell.matthew@gmail.com>
2017-10-26 19:21:13 -0400

    custom reduce working!

--- BEM/scriptTestCustomReduce.cpp
+++ BEM/scriptTestCustomReduce.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <omp.h>
 
+#include "matrixRoutines.hpp"
+
+#pragma omp declare reduction( + : Eigen::MatrixXd : omp_out = omp_out + omp_in) initializer(omp_priv = Eigen::MatrixXd::Zero(5,5))
+
 int main(){
 
-	
+	Eigen::MatrixXd result = Eigen::MatrixXd::Zero(5,5);
+
+	#pragma omp parallel for reduction( + : result)
+	for(size_t i = 0; i < 200; i++){
+		result += Eigen::MatrixXd::Identity(5,5);
+	}
+
+	printMatrix(result);
 
 }
